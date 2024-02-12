cdd6c03d8578d674eab68a7711e4d6efe02d73cb
unknown <lowell.lobo@hotmail.com>
2024-02-12 15:31:00 -0500

    Ultrasonic code

---
+++ ultrasonic/distance.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as gpio
+import time
+
+# import signal
+# import sys
+
+# def signal_handler(sig, frame):
+#     print('You pressed Ctrl+C!')
+#     sys.exit(0)
+
+# signal.signal(signal.SIGINT, signal_handler)
+# print('Press Ctrl+C')
+# signal.pause()
+
+trig = 16
+echo = 18
+
+# with this logic always remember to cleanup
+gpio.setmode(gpio.BOARD)
+gpio.setup(trig, gpio.OUT)
+gpio.setup(echo, gpio.IN)
+
+gpio.output(trig, False)
+time.sleep(0.01)
+
+def dist():
+    gpio.output(trig, True)
+    time.sleep(0.00001)
+    gpio.output(trig, False)
+
+    while gpio.input(echo) == 0:
+        start = time.time()
+
+    while gpio.input(echo) == 1:
+        end = time.time()
+
+    duration = end - start
+
+    distance = duration * 17150
+    distance = round(distance, 2)
+
+    return distance
+
+print(f"Distance {dist()} cm")
+gpio.cleanup()
+
