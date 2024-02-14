07fcc07d3cbfa0c5797a320e1f127860840d1c12
lorocks <lowell.lobo@hotmail.com>
2024-02-14 14:53:19 -0500

    Remake video wrte code

--- cvtest/cvwrite.py
+++ cvtest/cvwrite.py
@@ -1,62 +1,44 @@
-# Python program to save a  
-# video using OpenCV 
-  
-   
-import cv2 
-  
-   
-# Create an object to read  
-# from camera 
-video = cv2.VideoCapture(0) 
-   
-# We need to check if camera 
-# is opened previously or not 
-if (video.isOpened() == False):  
-    print("Error reading video file") 
-  
-# We need to set resolutions. 
-# so, convert them from float to integer. 
-frame_width = int(video.get(3)) 
-frame_height = int(video.get(4)) 
-   
-size = (frame_width, frame_height) 
-   
-# Below VideoWriter object will create 
-# a frame of above defined The output  
-# is stored in 'filename.avi' file. 
-result = cv2.VideoWriter('filename.avi',  
-                         cv2.VideoWriter_fourcc(*'MJPG'), 
-                         10, size) 
-    
-while(True): 
-    ret, frame = video.read() 
-  
-    if ret == True:  
-  
-        # Write the frame into the 
-        # file 'filename.avi' 
-        result.write(frame) 
-  
-        # Display the frame 
-        # saved in the file 
-        cv2.imshow('Frame', frame) 
-  
-        # Press S on keyboard  
-        # to stop the process 
-        if cv2.waitKey(1) & 0xFF == ord('s'): 
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
+
+size = (frame_width, frame_height)
+
+result = cv2.VideoWriter('video.avi',
+                         cv2.VideoWriter_fourcc(*'MJPG'),
+                         10, size)
+start = time.time()
+frame_rate_calc = 1
+while(True):
+    t1 = cv2.getTickCount()
+    ret, frame = video.read()
+
+    if ret:
+        cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                1, (255, 255, 0), 2,
+                cv2.LINE_AA)
+        result.write(frame)
+        cv2.imshow('Frame', frame)
+
+        t2 = cv2.getTickCount()
+        time1 = (t2 - t1) / cv2.getTickFrequency()
+        frame_rate_calc = 1 / time1
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-  
-    # Break the loop 
-    else: 
+
+        if time.time() - start > 10:
+              break
+
+    else:
         break
-  
-# When everything done, release  
-# the video capture and video  
-# write objects 
-video.release() 
-result.release() 
-    
-# Closes all the frames 
-cv2.destroyAllWindows() 
-   
-print("The video was successfully saved") 
+
+video.release()
+result.release()
+cv2.destroyAllWindows()
+
+print("The video was successfully saved")
