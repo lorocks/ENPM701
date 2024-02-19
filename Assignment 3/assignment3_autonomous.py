dcba2b3eadfb659bf80f4f6011c33b665e106f08
unknown <lowell.lobo@hotmail.com>
2024-02-19 15:12:57 -0500

    Add assignment 3 code

---
+++ Assignment 3/assignment3_autonomous.py
@@ -0,0 +1,71 @@
+import cv2
+import imutils
+import time
+import numpy as np
+import os
+
+cap = cv2.VideoCapture(0)
+
+
+time_vals = []
+
+# Define the lower and upper color thresholds
+lower = np.array([40, 60, 50])
+upper = np.array([95, 255, 255])
+lower = np.array([50, 70, 100])
+
+
+
+while cap.isOpened():
+    start = time.time()
+    # Read frame from the video
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Update object localizer
+    # frame = cv2.flip(frame, -1)
+
+    # Convert the image to the HSV color space
+    hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Create a mask based on the color thresholds
+    mask = cv2.inRange(hsv_image, lower, upper)
+
+    # Find contour
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Draw bounding box
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+    cv2.imshow("Video Stream", frame)
+    cv2.imshow("hsv", mask)
+
+    # record.write(image)
+
+    time1 = time.time() - start
+    time_vals.append(time1)
+
+    # Press key q to stop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cv2.destroyAllWindows()
+cap.release()
+
+print("Writing data to file")
+
+if os.path.exists("hw3data.txt"):
+  os.remove("hw3data.txt")
+else:
+  print("The file does not exist")
+
+f = open('hw3data.txt', 'a')
+for val in time_vals:
+  f.write(str(val))
+  f.write('\n')
+f.close()
--- cvtest/cv.py
+++ cvtest/cv.py
@@ -3,11 +3,18 @@ import time
 
 cap = cv2.VideoCapture(0)
 
-record = cv2.VideoWriter('video.avi',cv2.VideoWriter_fourcc(*'MJPG'), 10, (1280,720))
+# record = cv2.VideoWriter('video.avi',cv2.VideoWriter_fourcc(*'MJPG'), 10, (1280,720))
 
 start = time.time()
 
 frame_rate_calc = 1
+
+pic_cap = 0
+start = 0
+
+count = {
+	'a': "olaa"
+}
 
 while cap.isOpened():
 	t1 = cv2.getTickCount()
@@ -20,18 +27,29 @@ while cap.isOpened():
                 cv2.LINE_AA)
 	cv2.imshow("Frame", frame)
 
-	record.write(frame)
+	# record.write(frame)
 
 	t2 = cv2.getTickCount()
 	time1 = (t2 - t1) / cv2.getTickFrequency()
 	frame_rate_calc = 1 / time1
-    
-	if cv2.waitKey(1) & 0xFF == ord('q'):
+	
+	input = cv2.waitKey(1) & 0xFF
+	
+	if pic_cap == 0 and input != 255:
+		pic_cap = input
+		start = time.time()
+	
+	if time.time() - start > 10 and pic_cap != 0:
+		# save pic
+		print(count[chr(pic_cap)])
+		pic_cap = 0
+
+	if input == 9:
 		break
 
-	if time.time() - start > 10:
-		  break
+	# if time.time() - start > 10:
+	# 	  break
 
 cv2.destroyAllWindows()
 cap.release()
-record.release()
+# record.release()
