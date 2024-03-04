4f1119fe079019df71108ff94c886ff69d7f7212
unknown <lowell.lobo@hotmail.com>
2024-03-04 14:39:38 -0500

    Assignment 4

---
+++ Assignment 4/ultrasonic.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as gpio
+import time
+import cv2
+
+# with this logic always remember to cleanup
+trig = 16
+echo = 18
+
+gpio.setmode(gpio.BOARD)
+gpio.setup(trig, gpio.OUT)
+gpio.setup(echo, gpio.IN)
+
+gpio.output(trig, False)
+time.sleep(0.01)
+
+def dist():
+    gpio.output(trig, True)
+    time.sleep(0.00001)
+    gpio.output(trig, False)
+
+    while gpio.input(echo) == 0:
+        start = time.time()
+
+    while gpio.input(echo) == 1:
+        end = time.time()
+
+    duration = end - start
+
+    distance = duration * 17150
+    distance = round(distance, 2)
+
+    return distance
+
+distances = []
+
+for i in range(10):
+    distances.append(dist())
+
+cap = cv2.VideoCapture(0)
+
+ret, frame = cap.read()
+
+if ret:
+    cv2.putText(frame, f'Distance: {sum(dist)/len(dist)} cm', (30, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                1, (255, 255, 0), 2,
+                cv2.LINE_AA)
+    
+    cv2.imwrite("ultrasonic_dist.jpg", frame)
+
+else:
+    print("No camera connected")
