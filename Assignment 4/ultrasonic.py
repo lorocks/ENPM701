f6d57bab53b2961ff6f14969ad2cc2aa6e9d4d3a
unknown <lowell.lobo@hotmail.com>
2024-03-06 10:03:35 -0500

    Minor ultrasonic hw4 changes

--- Assignment 4/ultrasonic.py
+++ Assignment 4/ultrasonic.py
@@ -41,6 +41,7 @@ cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 
 if ret:
+    frame = cv2.flip(frame)
     cv2.putText(frame, f'Distance: {sum(dist)/len(dist)} cm', (30, 50), cv2.FONT_HERSHEY_SIMPLEX,
                 1, (255, 255, 0), 2,
                 cv2.LINE_AA)
@@ -49,3 +50,5 @@ if ret:
 
 else:
     print("No camera connected")
+
+cap.release()
