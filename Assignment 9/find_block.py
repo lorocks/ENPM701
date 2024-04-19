c630c270303df500a46f7d1abf15bfb26c328950
unknown <lowell.lobo@hotmail.com>
2024-04-19 11:52:13 -0400

    Fix green lower bound error

--- Assignment 9/find_block.py
+++ Assignment 9/find_block.py
@@ -286,6 +286,8 @@ try:
 
                 x_diff = 320 - x_centr
 
+                forward((120*8)/(2*3.1415*1.279528))
+
                 # if x_diff < 0:
                 #     right(abs(x_diff*0.0012))
                 # else:
