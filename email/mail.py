c5d602339e5c58a10d5adcf2fadc6843d2636054
unknown <lowell.lobo@hotmail.com>
2024-04-19 12:33:45 -0400

    Fix datetime rerror

--- email/mail.py
+++ email/mail.py
@@ -57,7 +57,7 @@ while cap.isOpened():
 
 
 def send_email(image):
-    time_now = datetime.now().strftime('%Y%m%d%H%M%S')
+    time_now = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 
     smtpUser = 'ENPM701.lorocks@gmail.com'
     smtpPass = 'jtesrcwsaygtxubj'
