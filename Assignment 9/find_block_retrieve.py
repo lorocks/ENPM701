2bee64de7e099516b7f33a20ada7beec9e22561b
unknown <lowell.lobo@hotmail.com>
2024-04-22 14:50:36 -0400

    Closer center

--- Assignment 9/find_block_retrieve.py
+++ Assignment 9/find_block_retrieve.py
@@ -376,7 +376,7 @@ try:
                 send_email(image)
                 break
 
-            if x > 320 - w/4 or x + w*3/4 < 320:
+            if x > 320 - w*4/5 or x + w*4/5 < 320:
                 ser.reset_input_buffer()
                 x_centr = x + (w/2)
 
