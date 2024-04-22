484d6751a1165455611e5730186bc3f4d522f386
unknown <lowell.lobo@hotmail.com>
2024-04-22 14:26:10 -0400

    IMU test code

---
+++ IMU/imu.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as gpio
+import time
+import cv2
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+printed = False
+
+count = 0
+
+print("Started serial thingies")
+while not printed:
+    if ser.in_waiting > 0:
+        count += 1
+        print(ser.readline())
+
+        if count > 10:
+            printed = True
+print("Serial cleaned")
